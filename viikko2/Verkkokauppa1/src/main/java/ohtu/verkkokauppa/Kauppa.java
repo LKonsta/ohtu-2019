228192a393d06acda4bcc131c04fc1a63ebd7a53
LKonsta <komppa.lehto@gmail.com>
2019-11-11 20:05:40 +0200

    tehtävä 9

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -8,10 +8,10 @@ public class Kauppa {
     private Viitegeneraattori viitegeneraattori;
     private String kaupanTili;
 
-    public Kauppa() {
-        varasto = Varasto.getInstance();
-        pankki = Pankki.getInstance();
-        viitegeneraattori = Viitegeneraattori.getInstance();
+    public Kauppa(Varasto v, Pankki p, Viitegeneraattori g) {
+        varasto = v;
+        pankki = p;
+        viitegeneraattori = g;
         kaupanTili = "33333-44455";
     }
 

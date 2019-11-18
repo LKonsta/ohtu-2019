6acdabeab0101d45864f9ee430ab698532735406
LKonsta <komppa.lehto@gmail.com>
2019-11-18 23:18:16 +0200

    viikko 3 tehtävä 10

--- viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -22,6 +22,13 @@ public class Stepdefs {
         WebElement element = driver.findElement(By.linkText("login"));       
         element.click();   
     }    
+
+    @Given("command new user is selected")
+    public void newUserIsSelected() {
+	driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));       
+        element.click(); 
+    }
     
     @When("correct username {string} and password {string} are given")
     public void correctUsernameAndPasswordAreGiven(String username, String password) {
@@ -54,6 +61,36 @@ public class Stepdefs {
         assertTrue(driver.getPageSource().contains(pageContent));
     }
     
+    @When("nonexistant username {string} and password {string} are given")
+    public void nonExistantUsernameAndPasswordAreGiven(String username, String password) {
+	logInWith(username, password);
+    }
+
+    @When("a valid username {string} and password {string} and matching password confirmation are entered")
+    public void validUsernameAndPasswordAreEntered(String username, String password) {
+	createUserWith(username, password, password);
+    }
+
+    @When("a valid username {string} and password {string} but not matching password confirmation {string} are entered")
+    public void validUsernameAndPasswordAndNotMatchingPasswordConfirmationAreEntered(String username, String password, String passwordConfirmation) {
+	createUserWith(username, password, passwordConfirmation);
+    }
+
+    @When("a invalid username {string} and password {string} and matching password confirmation are entered")
+    public void invalidUsernameAndPasswordAreEntered(String username, String password) {
+	createUserWith(username, password, password);
+    }
+
+    @Then("a new user is created")
+    public void newUserCreated() {
+	pageHasContent("Welcome to Ohtu Application!");
+    }
+
+    @Then("user is not created and error {string} is reported")
+    public void newUserNotCreated(String error) {
+	pageHasContent(error);
+    }
+
     @After
     public void tearDown(){
         driver.quit();
@@ -72,6 +109,16 @@ public class Stepdefs {
         element = driver.findElement(By.name("password"));
         element.sendKeys(password);
         element = driver.findElement(By.name("login"));
+        element.submit();  
+    } 
+    private void createUserWith(String username, String password, String passwordConfirmation) {
+        assertTrue(driver.getPageSource().contains("Create username and give password"));
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+	element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(passwordConfirmation);
         element.submit();  
     } 
 }

6a77133ed95af27f4050dd630cca37df161f6f2c
LKonsta <komppa.lehto@gmail.com>
2019-11-25 17:59:37 +0200

    viikko 4 tehtava 4

--- viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -127,6 +127,28 @@ public class KauppaTest {
         verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 5);   
     }
 
+    @Test
+    public void yksiOstostaJotaOnJaYksiJotaPoistetaanPaaytyttyaPankinMetodiaTilisiirtoKutsutaanOikeallaArvolla() {
+
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+
+        when(viite.uusi()).thenReturn(42);
+
+        Varasto varasto = mock(Varasto.class);
+        when(varasto.saldo(1)).thenReturn(10); 
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        Kauppa k = new Kauppa(varasto, pankki, viite);              
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+	k.lisaaKoriin(1);
+	k.poistaKorista(1);
+        k.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 5);   
+    }
+
 }
 
 
